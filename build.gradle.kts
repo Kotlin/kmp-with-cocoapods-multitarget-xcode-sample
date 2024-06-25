2ed813a6a0c688601d0e6fb24caea36829de1625
pahill <pamela.a.hill@gmail.com>
2024-06-25 14:12:06 +0200

    Update to Kotlin V2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("multiplatform").version("1.9.20-RC").apply(false)
-    kotlin("native.cocoapods").version("1.9.20-RC").apply(false)
+    kotlin("multiplatform").version("2.0.0").apply(false)
+    kotlin("native.cocoapods").version("2.0.0").apply(false)
 }
